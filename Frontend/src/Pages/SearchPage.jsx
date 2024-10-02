04c27829bafe76cb9ba5006d2328101db74130e9
Akash Sharma <107851253+Akash-Sharma-9804@users.noreply.github.com>
2024-10-02 17:50:51 +0530

    Update SearchPage.jsx

--- Frontend/src/Pages/SearchPage.jsx
+++ Frontend/src/Pages/SearchPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useContentStore } from "../Store/Content";
+import { useContentStore } from "../Store/Content.js";
 import Navbar from "../Components/Navbar";
 import { Search } from "lucide-react";
 import toast from "react-hot-toast";
