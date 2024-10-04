b706a976cd798fedd106110e1be69a666d77b88c
Akash Sharma <107851253+Akash-Sharma-9804@users.noreply.github.com>
2024-10-04 17:57:33 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -24,13 +24,13 @@ app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
 
-// if(ENV_VARS.NODE_ENV === "production"){
-//   app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+if(ENV_VARS.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
-//   app.get("*", (req ,res )=>{
-//       res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
-//   })
-// };
+  app.get("*", (req ,res )=>{
+      res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+  })
+};
 
 app.listen(PORT, () => {
   
