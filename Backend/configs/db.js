4ef001c2db8ba7a2838c97da3eed0dc95e38bd86
Bibhuti <bibhuti103singh@gmail.com>
2024-10-05 15:50:37 +0530

    chore: add console to check deploy

--- Backend/configs/db.js
+++ Backend/configs/db.js
@@ -1,17 +1,12 @@
 import mongoose from "mongoose";
 import { ENV_VARS } from "./envVars.js";
 
-export const connectDB = async ()=>{
-
-    try{
-        const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
-        console.log("MongoDb connected : " +conn.connection.host);
-      
-    }
-    catch(error){       
-        process.exit(1);
-    };
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
+    console.log("MongoDb connected : " + conn.connection.host);
+  } catch (error) {
+    console.log("mongoDb connect error");
+    process.exit(1);
+  }
 };
- 
-
- 
--- Backend/configs/envVars.js
+++ Backend/configs/envVars.js
@@ -1,14 +1,11 @@
 import dotenv from "dotenv";
 
-
 dotenv.config();
 
-
 export const ENV_VARS = {
-    MONGO_URI :  process.env.MONGO_URi,
-    PORT : process.env.PORT || 5000, 
-    JWT_SECRET : process.env.JWT_SECRET,
-    NODE_ENV :  process.env.NODE_ENV,
-    TMDB_API_KEY: process.env.TMDB_API_KEY,
+  MONGO_URI: process.env.MONGO_URi,
+  PORT: process.env.PORT || 5000,
+  JWT_SECRET: process.env.JWT_SECRET,
+  NODE_ENV: process.env.NODE_ENV,
+  TMDB_API_KEY: process.env.TMDB_API_KEY,
 };
-
