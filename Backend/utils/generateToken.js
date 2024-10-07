e7da4525b1b562b27ac6a2fc592ce111167ef6e9
Akash Sharma <akashsharma9804@gmail.com>
2024-10-07 20:35:27 +0530

    jwt

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -7,7 +7,7 @@ export const generateTokenAndSetCookie = (userId, res)=>{
         maxAge : 15*24*60*60*1000,
         httpOnly: true,
         sameSite: "strict",
-        secure: ENV_VARS.NODE_ENV !== "development",
+        secure: true,
     });
     return token;
 
